661a724973e043162dd0f2e731382474c5f716fa
Marianna Weychan <marianna.weychan@wp.pl>
2022-11-24 18:02:43 +0100

    created a currency calculator in react create app

---
+++ src/Container/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+
+const Container = ({ children }) => (
+    <body className="body">
+        {children}
+    </body>
+);
+
+export default Container;
---
+++ src/Form/index.js
@@ -0,0 +1,64 @@
+import "./style.css";
+import React, { useState } from "react";
+import { currencies } from "../Currencies/index.js";
+
+
+const currencySelection = currencies[0].short;
+
+const Form = () => {
+    const [yourAmount, setYourAmountExchange] = useState("");
+    const [currency, setCurrency] = useState(currencySelection);
+    const [result, setResult] = useState(null);
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(yourAmount, currency);
+    };
+
+    const calculateResult = (amountExchange, currency) => {
+        const { rate, short } = currencies.find(({ short }) => short === currency);
+
+        setResult(`${(+yourAmount / rate).toFixed(2)} ${short}`);
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Wybierz walutę, którą chcesz przeliczyć</legend>
+                <p>
+                    <label><span className="form__labelText">Twoja kwota w PLN:</span>
+                        <input
+                            className="form__field"
+                            type="number"
+                            min="0.01"
+                            step="any"
+                            value={yourAmount}
+                            onChange={({ target }) => setYourAmountExchange(target.value)}
+                            required
+                            autofocus
+                        />
+                    </label>
+                </p>
+                <label><span className="form__labelText"> Wybierz walutę:</span>
+                    <select
+                        className="form__field"
+                        name="currency"
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
+                    >
+                        {currencies.map((currency) => (
+                            <option key={currency.short} value={currency.short}>
+                                {currency.name}
+                            </option>
+                        ))};
+                    </select>
+                </label>
+            </fieldset>
+            <button className="form__button">Policz</button>
+            <p className="form__result">
+                Twoja kwota w wybranej walucie: <strong>{result}</strong></p>
+        </form>
+    );
+};
+
+export default Form;
