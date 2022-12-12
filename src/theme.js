c650fa0507a0900d71ba46476670616172e0cc22
Marianna Weychan <marianna.weychan@wp.pl>
2022-12-12 16:13:32 +0100

    theme added

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+export const theme = {
+    colors: {
+        tealBlue: "#054E50",
+        greenWhite: "#E7E8DE",
+        paradiso: "#358D8D",
+        white: "#FFFFFF",
+        halfBaked: "#90BECB",
+        astral: "#35819E",
+        botticelli: "#CEE6E8",
+        pelorous: "#2FA3B6",
+        downy: "#71C9CC",
+
+
+
+
+
+
+
+
+
+    },
+
+    breakpoint: {
+        mobile: 767
+    },
+};
