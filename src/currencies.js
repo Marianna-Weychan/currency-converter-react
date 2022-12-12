934f3b4e9c835d2359d1d90be645dda5dbdd9f38
Marianna Weychan <marianna.weychan@wp.pl>
2022-12-12 14:56:04 +0100

    renamed currencies to currencies.js

---
+++ src/currencies.js
@@ -0,0 +1,25 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.80
+    },
+
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.92
+    },
+
+    {
+        short: "CHF",
+        name: "Frank Szwajcarski",
+        rate: 4.88
+    },
+
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.48
+    },
+];
