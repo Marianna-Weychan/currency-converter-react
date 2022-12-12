bf16c5c29936b8d8f6260087877d7a64489da60b
Marianna Weychan <marianna.weychan@wp.pl>
2022-12-12 16:14:32 +0100

    formatDate moved over the Date function

--- src/Form/Date/index.js
+++ src/Form/Date/index.js
@@ -1,18 +1,18 @@
 import { useCurrentDate } from "./useCurrentDate";
 import { StyledDate } from "./styled";
 
+const formatDate = (currentDate) =>
+    currentDate.toLocaleString(undefined, {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    });
+
 const Date = () => {
     const currentDate = useCurrentDate();
-
-    const formatDate = (currentDate) =>
-        currentDate.toLocaleString(undefined, {
-            weekday: "long",
-            month: "long",
-            day: "numeric",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-        });
 
     return (
         <StyledDate>
