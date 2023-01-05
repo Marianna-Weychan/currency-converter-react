108b3a594d83a57dcf6fccc6d2f1a09d962ae69d
Marianna Weychan <marianna.weychan@wp.pl>
2023-01-05 13:08:44 +0100

    rebuilding the code for fetching data from the api

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,14 @@
-import { StyledForm, Fieldset, Legend, LabelText, StyledInput, Button, Result } from "./styled";
+import { StyledForm, Fieldset, Legend, LabelText, StyledInput, Button, Result, Info, Loading, Failure } from "./styled";
 import React, { useState } from "react";
-import { currencies } from "../currencies.js";
 import Date from "./Date";
+import { useRatesData } from "./useRatesData";
 
-const INITIAL_CURRENCY = currencies[0].short;
 
 const Form = () => {
     const [amount, setamountExchange] = useState("");
-    const [currency, setCurrency] = useState(INITIAL_CURRENCY);
-    const [result, setResult] = useState(null);
+    const [currency, setCurrency] = useState("EUR");
+    const [result, setResult] = useState();
+    const ratesData = useRatesData();
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -16,54 +16,72 @@ const Form = () => {
     };
 
     const calculateResult = (amount, currency) => {
-        const { rate, short } = currencies.find(({ short }) => short === currency);
+        const rate = ratesData.rates[currency];
 
-        setResult(`${(+amount / rate).toFixed(2)} ${short}`);
+        setResult(`${(+amount * rate).toFixed(2)} ${currency}`);
     };
 
     return (
         <StyledForm onSubmit={onFormSubmit}>
-            <Fieldset>
-                <Date />
-                <Legend>Wybierz walutę, którą chcesz przeliczyć</Legend>
-                <p>
-                    <label>
-                        <LabelText>Twoja kwota w PLN:</LabelText>
-                        <StyledInput
-                            type="number"
-                            min="0.01"
-                            step="any"
-                            value={amount}
-                            onChange={({ target }) => setamountExchange(target.value)}
-                            required
-                            autoFocus
-                        />
-                    </label>
-                </p>
-                <label><LabelText> Wybierz walutę:</LabelText>
-                    <StyledInput
-                        as="select"
-                        name="currency"
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency) => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}>
-                                {currency.name}
-                            </option>
-                        ))};
-                    </StyledInput>
-                </label>
-            </Fieldset>
-            <Button>Policz</Button>
-            <Result>
-                Twoja kwota w wybranej
-                walucie: <strong>{result}</strong>
-            </Result>
+            {ratesData.state === "loading"
+                ? (
+                    <Loading>
+                        Chwileczkę...⌛<br />Ładuję kursy z Europejskiego Banku Centralnego
+                    </Loading>
+                )
+                :
+                ratesData.state === "error" ? (
+                    <Failure>
+                        Ups... coś poszło nie tak 😐 <br />
+                        Sprawdź czy masz połączenie z internetem i spróbuj odświeżyć stronę przeglądarki.
+                    </Failure>
+                ) : (
+                    <>
+                        <Fieldset>
+                            <Date />
+                            <Legend>Wybierz walutę, którą chcesz przeliczyć</Legend>
+                            <p>
+                                <label>
+                                    <LabelText>Twoja kwota w PLN:</LabelText>
+                                    <StyledInput
+                                        type="number"
+                                        min="0.01"
+                                        step="any"
+                                        value={amount}
+                                        onChange={({ target }) => setamountExchange(target.value)}
+                                        required
+                                        autoFocus
+                                    />
+                                </label>
+                            </p>
+                            <label><LabelText> Wybierz walutę:</LabelText>
+                                <StyledInput
+                                    as="select"
+                                    name="currency"
+                                    value={currency}
+                                    onChange={({ target }) => setCurrency(target.value)}
+                                >
+                                    {Object.keys(ratesData.rates).map((currency) => (
+                                        <option
+                                            key={currency}
+                                            value={currency}>
+                                            {currency}
+                                        </option>
+                                    ))};
+                                </StyledInput>
+                            </label>
+                        </Fieldset >
+                        <Button>Policz</Button>
+                        <Result>
+                            Twoja kwota w wybranej
+                            walucie: <strong>{result}</strong>
+                        </Result>
+                        <Info>
+                            Kursy walut aktualne na dzień: {ratesData.date}
+                        </Info>
+                    </>
+                )}
         </StyledForm>
-    );
+    )
 };
-
-export default Form;
+export default Form
